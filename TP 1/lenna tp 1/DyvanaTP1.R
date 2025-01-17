77ca1a61f88be7ec79d89885dce7362aef9039d4
Dyva8 <dyvanaseunkam@gmail.com>
2025-01-17 18:32:52 +0000

    Update DyvanaTP1.R

--- TP 1/lenna tp 1/DyvanaTP1.R
+++ TP 1/lenna tp 1/DyvanaTP1.R
@@ -1,10 +1,7 @@
 
-# Définir le fichier PDF (assurez-vous que le fichier existe à cet emplacement)
-fichier1 <- "ehcvm_conso_bfa2021.csv"
-fichier2 <- "calorie_conversion_wa_2021.csv"
 # Lire le texte du PDF
-fich1 <- read.csv(fichier1)
-fich2 <- read.csv(fichier2)
+fich1 <- read.csv("calorie_conversion_wa_2021.csv")
+fich2 <- read.csv("ehcvm_conso_bfa2021.csv")
 # Afficher le texte extrait
 str(fich1)
 str(fich2)
