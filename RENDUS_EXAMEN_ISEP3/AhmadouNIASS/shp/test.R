80cf66faf1fe09786f7a3f6e4e8996ce46ef713d
Ahmadou Niass <149987722+ahmadouniass@users.noreply.github.com>
2025-05-10 20:54:19 +0000

    Delete AhmadouNIASS directory

--- RENDUS_EXAMEN_ISEP3/AhmadouNIASS/shp/test.R
+++
@@ -1,9 +0,0 @@
-library(sf)
-library(tmap)
-
-# Charger les régions (niveau administratif 1)
-regions <- st_read("tcd_admbnda_adm1_20250212_AB.shp")
-departements <- st_read("tcd_admbnda_adm2_20250212_AB.shp")
-print(st_geometry_type(regions))
-print(st_crs(regions))
-print(head(regions))
